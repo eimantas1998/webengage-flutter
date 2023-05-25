f7aa5cb2c59d71b84b8a181fd042b6d082831139
eimantas1998 <eimantas1998@gmail.com>
2023-05-25 16:28:32 +0300

    b

--- webengage_flutter/android/src/main/java/com/webengage/webengage_plugin/WebEngagePlugin.java
+++ webengage_flutter/android/src/main/java/com/webengage/webengage_plugin/WebEngagePlugin.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.firebase.messaging.FirebaseMessaging;
